56ef87c3f08bf63974872e661ee205ecb159f741
NightSkyZHX <2747684729@qq.com>
2023-09-13 21:21:57 +0800

    p5

--- p5_redis/src/bin/client.rs
+++ p5_redis/src/bin/client.rs
@@ -19,7 +19,7 @@ async fn main() {
         let mut flag = false;
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        let input = input.strip_suffix("\n").unwrap().to_string();
+        let input = input.strip_suffix("\n").unwrap().strip_suffix("\r").unwrap().to_string();
         let str_vec: Vec<String> = input.split(' ').map(|str| str.to_string()).collect();
         // println!("{:?}", &str_vec);
         let mut req = volo_gen::mini_redis::RedisRequest {
@@ -27,47 +27,55 @@ async fn main() {
             value: None,
             r#type: volo_gen::mini_redis::RequestType::Illegal,
         };
-        if str_vec[0] == "PING" {
-            // println!("{}", input.strip_prefix("PING ").unwrap().to_string());
-            req = volo_gen::mini_redis::RedisRequest {
-                key: None,
-                value: Some(input.strip_prefix("PING ").unwrap().to_string().into()),
-                r#type: volo_gen::mini_redis::RequestType::Ping,
-            }
-        } else if str_vec[0] == "DEL" {
-            let mut temp = vec![];
-            for i in 1..str_vec.len() {
-                temp.push(str_vec.get(i).unwrap().clone().into());
-            }
-            req = volo_gen::mini_redis::RedisRequest {
-                key: Some(temp),
-                value: None,
-                r#type: volo_gen::mini_redis::RequestType::Del,
-            }
-        } else if str_vec.len() == 2 {
-            if str_vec[0] == "GET" {
+        
+        match str_vec[0].as_str() {
+            "PING" => {
                 req = volo_gen::mini_redis::RedisRequest {
-                    key: Some(vec![str_vec.get(1).unwrap().clone().into()]),
+                    key: None,
+                    value: Some(input.strip_prefix("PING ").unwrap().to_string().into()),
+                    r#type: volo_gen::mini_redis::RequestType::Ping,
+                }
+            }
+            "DEL" => {
+                let mut temp = vec![];
+                for i in 1..str_vec.len() {
+                    temp.push(str_vec.get(i).unwrap().clone().into());
+                }
+                req = volo_gen::mini_redis::RedisRequest {
+                    key: Some(temp),
                     value: None,
-                    r#type: volo_gen::mini_redis::RequestType::Get,
+                    r#type: volo_gen::mini_redis::RequestType::Del,
                 }
             }
-        } else if str_vec.len() == 3 {
-            if str_vec[0] == "SET" {
+            "GET" => {
+                if str_vec.len() == 2 {
+                    req = volo_gen::mini_redis::RedisRequest {
+                        key: Some(vec![str_vec.get(1).unwrap().clone().into()]),
+                        value: None,
+                        r#type: volo_gen::mini_redis::RequestType::Get,
+                    }
+                }
+            }
+            "SET" => {
+                if str_vec.len() == 3 {
+                    req = volo_gen::mini_redis::RedisRequest {
+                        key: Some(vec![str_vec.get(1).unwrap().clone().into()]),
+                        value: Some(str_vec.get(2).unwrap().clone().into()),
+                        r#type: volo_gen::mini_redis::RequestType::Set,
+                    }
+                }
+            }
+            "EXIT" => {
+                flag = true;
                 req = volo_gen::mini_redis::RedisRequest {
-                    key: Some(vec![str_vec.get(1).unwrap().clone().into()]),
-                    value: Some(str_vec.get(2).unwrap().clone().into()),
-                    r#type: volo_gen::mini_redis::RequestType::Set,
+                    key: None,
+                    value: None,
+                    r#type: volo_gen::mini_redis::RequestType::Exit,
                 }
             }
-        } else if str_vec.len() == 1 && str_vec[0] == "EXIT" {
-            flag = true;
-            req = volo_gen::mini_redis::RedisRequest {
-                key: None,
-                value: None,
-                r#type: volo_gen::mini_redis::RequestType::Exit,
-            }
+            _ => {}
         }
+
         let resp = CLIENT.redis_command(req).await;
         if flag {
             process::exit(0);
